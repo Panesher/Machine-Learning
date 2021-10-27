e40c39f1d4465e2c350a5ca15dbff4001934d729
Panesher <paneshali3@gmail.com>
2021-10-27 23:48:01 +0300

    Tok

---
+++ HW-3/linear_regression.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from typing import List
+
+import numpy as np
+
+from descents import BaseDescent
+from descents import get_descent
+
+
+class LinearRegression:
+    """
+    Linear regression class
+    """
+
+    def __init__(self, descent_config: dict, tolerance: float = 1e-4, max_iter: int = 300):
+        """
+        :param descent_config: gradient descent config
+        :param tolerance: stopping criterion for square of euclidean norm of weight difference (float)
+        :param max_iter: stopping criterion for iterations (int)
+        """
+        self.descent: BaseDescent = get_descent(descent_config)
+
+        self.tolerance: float = tolerance
+        self.max_iter: int = max_iter
+
+        self.loss_history: List[float] = []
+
+    def fit(self, x: np.ndarray, y: np.ndarray) -> LinearRegression:
+        """
+        Fitting descent weights for x and y dataset
+        :param x: features array
+        :param y: targets array
+        :return: self
+        """
+        self.loss_history += [self.calc_loss(x, y)]
+        for iter in range(1, self.max_iter + 1):
+            diff = self.descent.update_weights(self.descent.calc_gradient(x, y))
+            self.loss_history += [self.calc_loss(x, y)]
+            if np.isnan(diff).any() or diff.dot(diff) < self.tolerance:
+                break
+        assert False, f'funny bunny {len(self.loss_history), max_iter, diff.dot(diff), self.tolerance}'
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        """
+        Predicting targets for x dataset
+        :param x: features array
+        :return: prediction: np.ndarray
+        """
+        return self.descent.predict(x)
+
+    def calc_loss(self, x: np.ndarray, y: np.ndarray) -> float:
+        """
+        Calculating loss for x and y dataset
+        :param x: features array
+        :param y: targets array
+        """
+        return self.descent.calc_loss(x, y)
